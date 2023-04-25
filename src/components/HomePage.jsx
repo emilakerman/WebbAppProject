5321ac8db12ba99bafea3d4faeda0ff2adc44608
Emil Åkerman <emil.akerman@iths.se>
2023-04-25 10:46:32 +0200

    added some initial code for the moviethumb.jsx + css

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import MovieThumb from "./MovieThumb";
+import { useState, useEffect } from "react";
 
 
 const HomePage = () => {
-
+    return (
+        <MovieThumb />
+    )
 }
 
 export default HomePage;
--- src/components/MovieThumb.jsx
+++ src/components/MovieThumb.jsx
@@ -1,7 +1,23 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import '.././App.css'
 
 
 const MovieThumb = () => {
-
+    return (
+        <div className='movie'>
+            <div>
+                <p>year or something</p>
+            </div>
+            <div>
+                <img src={'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/mIBCtPvKZQlxubxKMeViO2UrP3q.jpg'} alt='Movie title'/>
+            </div>
+            <div>
+                <h3>title test</h3>
+                <span>genre test</span>
+            </div>
+        </div>
+    )
 }
 
 export default MovieThumb;
