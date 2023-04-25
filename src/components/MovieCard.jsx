7b262f91d3de63af84f49bcf5c55b769e772d0ee
Emil Åkerman <emil.akerman@iths.se>
2023-04-25 13:40:44 +0200

    made a mobile friendly version of the single card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,8 +10,8 @@ const MovieCard = () => {
           <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vn1pWmu9G95wpapITYjhdKKcrvD.jpg" alt="Image"/>
         </div>
         <div className="rightSection">
-          <h2>Movie Title 2023</h2>
-          <p>Longer movie description about some stuff that I probablby wont read bit ts good to put in the code anyway I think.</p>
+          <h2 className='movie-tagline'>Movie Title 2023</h2>
+          <p className='movie-info'>Longer movie description about some stuff that I probablby wont read bit ts good to put in the code anyway I think.</p>
           <h3>Genre</h3>
           <h3>Release date</h3>
           <h3>Average rating</h3>
