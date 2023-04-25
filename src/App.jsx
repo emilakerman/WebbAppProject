e0eb6aeb72e98174dae7538da67aed80835d3812
AkselLofstedtNielsen <aksel98@outlook.com>
2023-04-25 14:42:45 +0200

    VideoPlayer with basic css

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,74 @@
 import './App.css';
 import handleSubmit from './handles/handlesubmit';
 import { useRef } from 'react';
+import VideoPlayer from './components/VideoPlayer';
+import video from "./assets/sample_mp4.mp4";
+
 
 const App = () => {
 
-  const dataRef = useRef()
+  const videoElement = useRef(null);
+  const {
+    playerState,
+    togglePlay,
+    handleOnTimeUpdate,
+    handleVideoProgress,
+    handleVideoSpeed,
+    toggleMute,
+  } = VideoPlayer(videoElement);
+
+  return (
+    <div className='container'>
+      <div className='video-wrapper'>
+        <video 
+          src={video}
+          ref={videoElement}
+          onTimeUpdate={handleOnTimeUpdate}
+        />
+        <div className='controls'>
+          <div className='actions'>
+            <div onClick={togglePlay} className='play-pause'>
+              {playerState.isPlaying ? 'Pause' : 'Play'}
+            </div>
+          </div>
+          <input 
+            type="range"
+            min="0"
+            max="100"
+            value={playerState.progress}
+            onChange={(e) => handleVideoProgress(e)} 
+          />
+          <select
+            className='velocity' 
+            value={playerState.speed}
+            onChange={(e) => handleVideoSpeed(e)}
+          >
+            <option value="0.50">0.50x</option>
+            <option value="1">1x</option>
+            <option value="1.25">1.25x</option>
+            <option value="1.5">1.5x</option>
+            <option value="2">2x</option>
+          </select>
+          <div onClick={toggleMute} className="mute-btn">
+            {playerState.isMuted ? 'Unmute' : 'Mute'}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  
+
+
+
+
+
+
+
+
+
+/* Firebase test */
+
+/*   const dataRef = useRef()
  
   const submithandler = (e) => {
     e.preventDefault()
@@ -19,7 +83,7 @@ return (
           <button type = "submit">Save</button>
         </form>
       </div>
-    );
+    ); */
 }
 
 export default App
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,7 +1,78 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 
-const VideoPlayer = () => {
+const VideoPlayer = (videoContent) => {
+    const [playerState, setPlayerState] = useState({
+        isPlaying : false,
+        progress: 0,
+        speed: 1,
+        isMuted : false
+    });
 
-}
+    const togglePlay = () => {
+        setPlayerState({
+            ...playerState,
+            isPlaying : !playerState.isPlaying,
+        });
+    };
+
+    useEffect(() => {
+        //om den spelas så pausa annars spela första gången den skapas
+        playerState.isPlaying ? videoContent.current.play() : videoContent.current.pause();
+    }, [playerState.isPlaying, videoContent]);
+
+    const handleOnTimeUpdate = () => {
+        //video progressbar
+        const progress = (videoContent.current.currentTime / videoContent.current.duration) * 100;
+        setPlayerState({
+            ...playerState,
+            progress,
+        });
+    };
+
+    const handleVideoProgress = (event) => {
+        //så man kan ändra tid genom progressbaren
+        const manualChange = Number(event.target.value);
+        videoContent.current.currentTime = (videoContent.current.duration / 100) * manualChange;
+        setPlayerState({
+            ...playerState,
+            progress: manualChange,
+        });
+    };
+
+    const handleVideoSpeed = (event) => {
+        //ändra hastigheten 1.0x = 1.25x tex
+        const speed = Number(event.target.value);
+        videoContent.current.playbackRate = speed;
+        setPlayerState({
+            ...playerState,
+            speed,
+        });
+    };
+
+    const toggleMute = () => {
+        //stäng av / på ljudet
+        setPlayerState({
+            ...playerState,
+            isMuted: !playerState.isMuted,
+        });
+    };
+
+    useEffect(() => {
+         playerState.isMuted ? (videoContent.current.muted = true) : (videoContent.current.muted = false);
+        
+    }, [playerState.isMuted, videoContent]);
+
+    return {
+        playerState,
+        togglePlay,
+        handleOnTimeUpdate,
+        handleVideoProgress,
+        handleVideoSpeed,
+        toggleMute
+    };
+   
+};
 
 export default VideoPlayer;
