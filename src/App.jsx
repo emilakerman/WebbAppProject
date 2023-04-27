c8e837ad750a3c847236f176c88c9e5572c6a610
Emil Åkerman <emil.akerman@iths.se>
2023-04-27 10:01:48 +0200

    commented out some unused code in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,46 @@ import { useState, useEffect } from 'react';
 import './App.css'
 import axios from 'redaxios'; //external api fetch library (replacing fetch, this is more light weight apparently)
 
+//Omdb api url
 let apiURL = "http://www.omdbapi.com/?apikey=9875f2c9";
 
 //TMD api key
 //128373ab4341186161d282674c1d9e7b
 
-//this GET gives the trending movies of the week
-/*
-https://api.themoviedb.org/3/trending/movie/week?api_key=128373ab4341186161d282674c1d9e7b
-*/
 
-let title = "Batman"; //just using this now for static testing
+
+// let title = "Batman"; //just using this now for static testing
 
 const App = () => {
 
-  useEffect(() => {
-    fetchData(title);
-  }, []);
+//   useEffect(() => {
+//     fetchData(title);
+//   }, []);
 
-  const fetchData = (title) => {
-    axios.get(`${apiURL}&s=${title}`)
-    .then((response) => {
-      for (const movie in response.data.Search) {
-        console.log(response.data.Search[movie].Title) //logs out a bunch of batman movies
-      }
-    })
-    .catch((error) => {
-      ///various error handling
-        if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-        } else if (error.request) {
-        console.log(error.request);
-        } else {
-        console.log('Error', error.message);
-        }
+//   const fetchData = (title) => {
+//     axios.get(`${apiURL}&s=${title}`)
+//     .then((response) => {
+//       for (const movie in response.data.Search) {
+//         console.log(response.data.Search[movie].Title) //logs out a bunch of batman movies
+//       }
+//     })
+//     .catch((error) => {
+//       ///various error handling
+//         if (error.response) {
+//         console.log(error.response.data);
+//         console.log(error.response.status);
+//         console.log(error.response.headers);
+//         } else if (error.request) {
+//         console.log(error.request);
+//         } else {
+//         console.log('Error', error.message);
+//         }
     
-    })
-    .finally(function () {
-        // always executed, not used right now
-    }); 
-}
+//     })
+//     .finally(function () {
+//         // always executed, not used right now
+//     }); 
+// }
 
   return(
     <h1>hej</h1>
