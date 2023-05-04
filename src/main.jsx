bf1ef7b988ec5248c5c19b7111f364011352f4dc
Filip Kostic <kosticfilip99@gmail.com>
2023-05-04 12:26:55 +0200

    adaptation of cardview

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
        {/* <App /> */}
-       {/* <HomePage /> */}
+      <HomePage /> 
        <MovieCard />
     </Router>
 
