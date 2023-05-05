084b6cabaf1f5508302c4932c74b9d4760aee53f
AkselLofstedtNielsen <aksel98@outlook.com>
2023-05-05 11:24:30 +0200

    Fixed logOut

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,11 +1,20 @@
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const LogIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [user, setUser] = useState(null); // state to keep track of user object
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      console.log("Changed user state useEffect")
+      setUser(user); // set user state to user object or null if user is not logged in
+    });
+    return unsubscribe;
+  }, []);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -47,24 +56,45 @@ const LogIn = () => {
       });
   };
 
+  const handleLogOut = () => {
+    const auth = getAuth();
+    auth.signOut().then(() => {
+      // signed out
+      alert("Logged Out");
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode + errorMessage);
+      setError(errorMessage);
+    });
+  };
+
   return (
     <div>
-      <label>
-        Email:
-        <input type="text" value={email} onChange={handleEmailChange} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={handlePasswordChange} />
-      </label>
-      <br />
-      <button onClick={handleLogIn}>Log In</button>
-      <button onClick={handleSignUp}>Sign Up</button>
-      {error && <p>{error}</p>}
+      {user ? (
+        <div>
+          <p>{user.email}</p>
+          <button onClick={handleLogOut}>Log Out</button>
+        </div>
+      ) : (
+        <div>
+          <label>
+            Email:
+            <input type="text" value={email} onChange={handleEmailChange} />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type="password" value={password} onChange={handlePasswordChange} />
+          </label>
+          <br />
+          <button onClick={handleLogIn}>Log In</button>
+          <button onClick={handleSignUp}>Sign Up</button>
+          {error && <p>{error}</p>}
+        </div>
+      )}
     </div>
   );
 };
 
 export default LogIn;
-
