90094516f3332750545df41128fc590c23ed675a
AkselLofstedtNielsen <aksel98@outlook.com>
2023-05-08 13:20:28 +0200

    Fixed cart w/o delete func

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,53 +1,57 @@
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore, doc, collection, getDocs } from "firebase/firestore";
 
 const ShoppingCart = () => {
-const [cartItems, setCartItems] = useState([
-    //Exempel filmer/pris, dem riktiga läggs till senare.
-        { title: "Movie 1 example", price: 4 },
-        { title: "Movie 2 example", price: 3 },
-        { title: "Movie 3 example", price: 6 }
+  const [shoppingCart, setShoppingCart] = useState([]);
+  const db = getFirestore();
 
-]);
+  useEffect(() => {
+    const fetchShoppingCart = async () => {
+      const user = getAuth().currentUser;
+      if (user) {
+        const userRef = doc(db, "users", user.uid);
+        const rentedMoviesRef = collection(userRef, "rentedMovies");
+        const snapshot = await getDocs(rentedMoviesRef);
+        const cartItems = snapshot.docs.map((doc) => doc.data());
+        setShoppingCart(cartItems);
+      }
+    };
 
-const removeMovie = (index) => {
-    const newCartItems = [...cartItems];
-    newCartItems.splice(index, 1)
-    setCartItems(newCartItems);
-};
+    fetchShoppingCart();
+  }, [db]);
 
+  const removeMovie = (movieId) => {
+    console.log(movieId);
+    const updatedCartItems = shoppingCart.filter((item) => item.id !== movieId);
+    setShoppingCart(updatedCartItems);
+  };
 
-const totalPrice = cartItems.reduce((total, movie) => total + movie.price, 0);
-return (
+  const totalPrice = shoppingCart.reduce((total, movie) => total + movie.price, 0);
+
+  return (
     <div>
-        <h2>Shopping Cart</h2>
-        <h3>Your added movies</h3>
-        <ul>
-            {cartItems.map ((movie, index) => (
-                <li key={index}>
-                    {movie.title} - {movie.price} $
-                    <button onClick={() => removeMovie(index)}>-</button>
-                </li>
+      <h2>Shopping Cart</h2>
+      {shoppingCart.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <div>
+          <h3>Your added movies</h3>
+          <ul>
+            {shoppingCart.map((movie) => (
+              <li key={movie.id}>
+                {movie.title} - {movie.price} $
+                <button onClick={() => removeMovie(movie.id)}>Remove</button>
+              </li>
             ))}
-        </ul>
-        <button>Add more movies</button>
-        <p>Total price: {totalPrice} $</p>
-        <button>Go to checkout</button>
+          </ul>
+          <button>Add more movies</button>
+          <p>Total price: {totalPrice} $</p>
+          <button>Go to checkout</button>
+        </div>
+      )}
     </div>
-)
-
-    const auth = getAuth();
-    const user = auth.currentUser;
-
-    if (user) {
-    // User signed in. TODO: Get users cart.
-    } else {
-    // No user is signed in: Empty cart
-    }
-
-
-}
+  );
+};
 
 export default ShoppingCart;
