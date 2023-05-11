c7142d240c06eecdc9bf3f560be4914cfbdbea6c
AkselLofstedtNielsen <aksel98@outlook.com>
2023-05-11 11:07:10 +0200

    FIxed delete movie

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getAuth } from "firebase/auth";
-import { getFirestore, doc, collection, getDocs } from "firebase/firestore";
+import { getFirestore, doc, collection, getDocs, deleteDoc } from "firebase/firestore";
+import { Link } from "react-router-dom"
 
 const ShoppingCart = () => {
   const [shoppingCart, setShoppingCart] = useState([]);
@@ -14,7 +15,6 @@ const ShoppingCart = () => {
         const rentedMoviesRef = collection(userRef, "rentedMovies");
         const snapshot = await getDocs(rentedMoviesRef);
         const cartItems = snapshot.docs.map((doc) => doc.data());
-        console.log(cartItems);
         setShoppingCart(cartItems);
       }
     };
@@ -22,19 +22,43 @@ const ShoppingCart = () => {
     fetchShoppingCart();
   }, [db]);
 
-  const removeMovie = async (movie) => {
-    //todo: Remove movie
+  const removeMovie = async (movieTitle) => {
+    try {
+      const user = getAuth().currentUser;
+      if (user) {
+        const userRef = doc(db, "users", user.uid);
+        const rentedMoviesRef = collection(userRef, "rentedMovies");
+        const querySnapshot = await getDocs(rentedMoviesRef);
 
+        querySnapshot.forEach((doc) => {
+          const movieData = doc.data();
+          if (movieData.title === movieTitle) {
+            deleteDoc(doc.ref);
+            console.log("Movie deleted!");
+          }
+        });
+
+        const updatedSnapshot = await getDocs(rentedMoviesRef);
+        const updatedCartItems = updatedSnapshot.docs.map((doc) => doc.data());
+        setShoppingCart(updatedCartItems);
+      }
+    } catch (error) {
+      console.error("Error removing movie:", error);
+    }
   };
 
-  //todo- movie price/total price
-  const totalPrice = 0
+  const totalPrice = 0;
 
   return (
     <div>
       <h2>Shopping Cart</h2>
       {shoppingCart.length === 0 ? (
-        <p>Your cart is empty.</p>
+        <p>Your cart is empty. <br/>
+          <button>
+            <Link to="/">Rent more movies</Link>
+          </button>
+        </p>
+        
       ) : (
         <div>
           <h3>Your added movies</h3>
@@ -46,7 +70,11 @@ const ShoppingCart = () => {
               </li>
             ))}
           </ul>
-          <button>Add more movies</button>
+
+          <button>
+          <Link to="/">Rent more movies</Link>
+          </button>
+          
           <p>Total price: {totalPrice} $</p>
           <button>Go to checkout</button>
         </div>
