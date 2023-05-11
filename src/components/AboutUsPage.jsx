a0b54916b9f6dfe07d07126824059c627cdd423c
Jonas B <jonas.backas@iths.se>
2023-05-11 09:20:34 +0200

    påbörjade lite med info page

--- src/components/AboutUsPage.jsx
+++ src/components/AboutUsPage.jsx
@@ -1,3 +1,30 @@
-export default function AboutUsPage() {
-  return <h1>hej</h1>
+import React from 'react';
+
+function AboutUsPage() {
+  const contacts = [
+    {
+      name: 'Big Buster Renthouse',
+      email: 'bb-renthouse@gmail.com',
+      phone: '555-578-55325',
+      Location: 'Kolarängsvägen 67 / 17843 Stockholm'
+    }
+  ];
+
+  return (
+    <div>
+      <h1>Contact Information</h1>
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact.email}>
+            <h2>{contact.name}</h2>
+            <p>Email: {contact.email}</p>
+            <p>Phone: {contact.phone}</p>
+            <p>Location: {contact.Location}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default AboutUsPage;
