d16aec3f880de68e634d5655b50ef35e9e707a2b
Emil Åkerman <emil.akerman@iths.se>
2023-05-15 10:13:39 +0200

    imported firebase storage in firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "@firebase/firestore"
+import { getStorage } from "firebase/storage"
 
 
 // TODO: Private .env file for the secret keys
@@ -17,3 +18,4 @@ const firebaseConfig = {
 // Initialize Firebase
  const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app)
+export const storage = getStorage(app);
