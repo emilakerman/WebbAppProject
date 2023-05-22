d35fe0c18f7d8b1402d746b2d587114281fba8ae
AkselLofstedtNielsen <aksel98@outlook.com>
2023-05-22 15:09:58 +0200

    Fixed 24H rent time

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import '.././payment.css'
 import { Link } from 'react-router-dom';
-import { getFirestore, doc, collection, getDocs, addDoc, deleteDoc, Timestamp } from "firebase/firestore";
+import { getFirestore, doc, collection, getDocs, addDoc, deleteDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-
-
+import '.././payment.css';
 
 const Payment = () => {
   const [cardNumber, setCardNumber] = useState('');
@@ -12,7 +10,6 @@ const Payment = () => {
   const [expirationYear, setExpirationYear] = useState('');
   const [cvc, setCvc] = useState('');
   const [cardholderName, setCardholderName] = useState('');
-
   const [shoppingCart, setShoppingCart] = useState([]);
   const db = getFirestore();
 
@@ -32,57 +29,39 @@ const Payment = () => {
   }, [db]);
 
   const addMoviesToBought = async () => {
-    
-    const currentDateAndTime = getCurrentDateTime();
-
     const user = getAuth().currentUser;
     if (user) {
       const userRef = doc(db, "users", user.uid);
       const rentedMoviesRef = collection(userRef, "RentedMovies");
-      const shoppingCartRef = collection(userRef, "shoppingCart")
+      const shoppingCartRef = collection(userRef, "shoppingCart");
   
-      // Get the movies from the shopping cart and set a timeStamp to when each movie was rented
+      const currentTime = new Date();
+      const expiryTime = new Date(currentTime.getTime() + 24 * 60 * 60 * 1000); // 1 day expiry
+
       const moviesToAdd = shoppingCart.map((movie) => ({
         title: movie.title,
-        time: currentDateAndTime
-
+        rentedAt: currentTime,
+        expiryDate: expiryTime,
       }));
   
-      // Add the movies to the rented movies collection
       try {
-        await Promise.all(moviesToAdd.map((movie) => addDoc(rentedMoviesRef, movie)));
+        await Promise.all(moviesToAdd.map(async (movie) => {
+          await addDoc(rentedMoviesRef, movie);
+        }));
         console.log("Movies added to the rented movies collection.");
-        // Clear the shopping cart after success
         setShoppingCart([]);
         const snapshot = await getDocs(shoppingCartRef);
         snapshot.forEach(async (doc) => {
           await deleteDoc(doc.ref);
         });
   
-
-        console.log("Firebase ShoppingCart cleared")
-        
+        console.log("Firebase ShoppingCart cleared");
       } catch (error) {
         console.error("Error adding movies to the rented movies collection:", error);
       }
     }
   };
-
-  const getCurrentDateTime = () => {
-    const date = new Date();
-    const year = String(date.getFullYear());
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
   
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-  };
-  
-  
-
-
   const handleCardNumberChange = (event) => {
     const input = event.target.value
       .replace(/\D/g, '') 
@@ -92,7 +71,6 @@ const Payment = () => {
     setCardNumber(input);
   };
   
-
   const handleExpirationMonthChange = (event) => {
     setExpirationMonth(event.target.value);
   };
@@ -116,99 +94,100 @@ const Payment = () => {
     // handle payment submission logic here
   };
 
-  
-
   return (
-    <div >
-    <div className="payment-form">
-    <form onSubmit={handleSubmit}>
-      <label className='cardNumberLabel' htmlFor="cardNumber">Card Number:</label>
-      <input className='cardNumber'
-        type="text"
-        id="cardNumber"
-        name="cardNumber"
-        value={cardNumber}
-        onChange={handleCardNumberChange}
-        placeholder="Enter card number"
-        maxLength="19"
-        required
-      />
+    <div>
+      <div className="payment-form">
+        <form onSubmit={handleSubmit}>
+          <label className='cardNumberLabel' htmlFor="cardNumber">Card Number:</label>
+          <input
+            className='cardNumber'
+            type="text"
+            id="cardNumber"
+            name="cardNumber"
+            value={cardNumber}
+            onChange={handleCardNumberChange}
+            placeholder="Enter card number"
+            maxLength="19"
+            required
+          />
 
-      <label className='expirationDateLabel' htmlFor="expirationMonth">Expiration Date:</label>
-      <div className='expirationDate'>
-        <select
-          id="expirationMonth"
-          name="expirationMonth"
-          value={expirationMonth}
-          onChange={handleExpirationMonthChange}
-          required
-        >
-          <option value="">Month</option>
-          <option value="01">01</option>
-          <option value="02">02</option>
-          <option value="03">03</option>
-          <option value="04">04</option>
-          <option value="05">05</option>
-          <option value="06">06</option>
-          <option value="07">07</option>
-          <option value="08">08</option>
-          <option value="09">09</option>
-          <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-        </select>
-        <select
-          id="expirationYear"
-          name="expirationYear"
-          value={expirationYear}
-          onChange={handleExpirationYearChange}
-          required
-        >
-          <option value="">Year</option>
-          <option value="2023">2023</option>
-          <option value="2024">2024</option>
-          <option value="2025">2025</option>
-          <option value="2026">2026</option>
-          <option value="2027">2027</option>
-          <option value="2027">2028</option>
-          <option value="2027">2029</option>
-          <option value="2027">2030</option>
-        </select>
+          <label className='expirationDateLabel' htmlFor="expirationMonth">Expiration Date:</label>
+          <div className='expirationDate'>
+            <select
+              id="expirationMonth"
+              name="expirationMonth"
+              value={expirationMonth}
+              onChange={handleExpirationMonthChange}
+              required
+            >
+              <option value="">Month</option>
+              <option value="01">01</option>
+              <option value="02">02</option>
+              <option value="03">03</option>
+              <option value="04">04</option>
+              <option value="05">05</option>
+              <option value="06">06</option>
+              <option value="07">07</option>
+              <option value="08">08</option>
+              <option value="09">09</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+            </select>
+            <select
+              id="expirationYear"
+              name="expirationYear"
+              value={expirationYear}
+              onChange={handleExpirationYearChange}
+              required
+            >
+              <option value="">Year</option>
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
+              <option value="2025">2025</option>
+              <option value="2026">2026</option>
+              <option value="2027">2027</option>
+              <option value="2027">2028</option>
+              <option value="2027">2029</option>
+              <option value="2027">2030</option>
+            </select>
+          </div>
+
+          <label className='cvcLabel' htmlFor="cvc">CVC:</label>
+          <input
+            className='cvc'
+            type="text"
+            id="cvc"
+            name="cvc"
+            value={cvc}
+            onChange={handleCvcChange}
+            placeholder="Enter CVC"
+            maxLength="3"
+            required
+          />
+
+          <label className='cardHolderNameLabel'htmlFor="cardholderName">Cardholder Name:</label>
+          <input
+            className='cardHolderName'
+            type="text"
+            id="cardholderName"
+            name="cardholderName"
+            value={cardholderName}
+            onChange={handleCardholderNameChange}
+            placeholder="Enter cardholder name"
+            maxLength="50"
+            required
+            pattern="^[^0-9]+$"
+          />
+
+          <Link to={'/StreamMoviePage'}>
+            <button className='payButton' type="submit" onClick={addMoviesToBought}>Pay Now</button>
+          </Link>
+
+        </form>
       </div>
-
-      <label className='cvcLabel' htmlFor="cvc">CVC:</label>
-      <input className='cvc'
-        type="text"
-        id="cvc"
-        name="cvc"
-        value={cvc}
-        onChange={handleCvcChange}
-        placeholder="Enter CVC"
-        maxLength="3"
-        required
-      />
-
-      <label className='cardHolderNameLabel'htmlFor="cardholderName">Cardholder Name:</label>
-      <input className='cardHolderName'
-    type="text"
-    id="cardholderName"
-    name="cardholderName"
-    value={cardholderName}
-    onChange={handleCardholderNameChange}
-    placeholder="Enter cardholder name"
-    maxLength="50"
-    required
-    pattern="^[^0-9]+$"
-  />
-
-<Link to={'/StreamMoviePage'}>
-  <button className='payButton' type="submit" onClick={addMoviesToBought}>Pay Now</button>
-</Link>
-
-</form>
-</div>
-</div>
-);
+    </div>
+  );
 };
 
 export default Payment;
--- src/components/StreamMoviePage.jsx
+++ src/components/StreamMoviePage.jsx
@@ -1,29 +1,77 @@
 
 import VideoPlayer from './VideoPlayer';
 import video from "../assets/sample_mp4.mp4";
-import { useEffect, useRef, useState } from 'react';
-import { getAuth } from "firebase/auth";
-import { getFirestore, doc, collection, getDocs } from 'firebase/firestore';
+import React, { useEffect, useRef, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, doc, collection, getDocs, deleteDoc } from 'firebase/firestore';
 
-    const StreamMoviePage = () => {
-    
-    const [rentedMovies, setRentedMovies] = useState([]);
-    const db = getFirestore();
+const StreamMoviePage = () => {
+  const [rentedMovies, setRentedMovies] = useState([]);
+  const db = getFirestore();
 
-    useEffect(() => {
-      const getRentedMovies = async () => {
+  useEffect(() => {
+    const getRentedMovies = async () => {
+      const user = getAuth().currentUser;
+      if (user) {
+        const userRef = doc(db, 'users', user.uid);
+        const rentedMoviesRef = collection(userRef, 'RentedMovies');
+        const snapshot = await getDocs(rentedMoviesRef);
+        const movies = snapshot.docs.map((doc) => doc.data());
+        setRentedMovies(movies);
+      }
+    };
+
+    getRentedMovies();
+  }, [db]);
+
+  useEffect(() => {
+    console.log("Minute");
+    const intervalId = setInterval(() => {
+      rentedMovies.forEach((movie) => {
+        console.log("foreach")
+        checkMovieExpiry(movie);
+      });
+    }, 60000); // Run every minute (60000 milliseconds)
+
+    return () => {
+      clearInterval(intervalId); // Cleanup interval when component unmounts
+    };
+  }, [rentedMovies]);
+
+  const checkMovieExpiry = async (movie) => {
+    console.log("Expiry")
+    const currentTimestamp = Date.now();
+    const expiryTimestamp = movie.expiryDate.toMillis();
+    console.log(currentTimestamp);
+    console.log(expiryTimestamp);
+  
+    if (currentTimestamp > expiryTimestamp) {
+      try {
         const user = getAuth().currentUser;
         if (user) {
-          const userRef = doc(db, "users", user.uid);
-          const rentedMoviesRef = collection(userRef, "RentedMovies");
-          const snapshot = await getDocs(rentedMoviesRef);
-          const movies = snapshot.docs.map((doc) => doc.data());
-          setRentedMovies(movies);
+          const userRef = doc(db, 'users', user.uid);
+          const rentedMoviesRef = collection(userRef, 'RentedMovies');
+          const querySnapshot = await getDocs(rentedMoviesRef);
+  
+          querySnapshot.forEach(async (doc) => {
+            const movieData = doc.data();
+            if (movieData.title === movie.title) {
+              await deleteDoc(doc.ref);
+              console.log(`Movie "${movie.title}" has expired and has been deleted from Firebase.`);
+              setRentedMovies((prevMovies) =>
+                prevMovies.filter((m) => m.title !== movie.title)
+              );
+            }
+          });
         }
-      };
-
-      getRentedMovies();
-    }, [db]);
+      } catch (error) {
+        console.error('Error deleting expired movie:', error);
+      }
+    }
+  };
+  
+  
+  
 
     
     const videoElement = useRef(null);
@@ -47,10 +95,11 @@ import { getFirestore, doc, collection, getDocs } from 'firebase/firestore';
             <h3>Rented Movies:</h3>
             <ul>
               {rentedMovies.map((movie) => (
-                <li key = {movie.id}>
-                  {movie.title}
-                  <h4>Rented at : {movie.time}</h4>
-                </li>
+            <li key={movie.id}>
+            {movie.title}
+            <p>Rented at: {movie.rentedAt.toDate().toString()}</p>
+            <p>Expires on: {movie.expiryDate.toDate().toString()}</p>
+          </li>
               ))}
             </ul>
           </div>
